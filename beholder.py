a1afb0ebf782a3eab5b3905b148a5db45f0962f1
IgorBeHolder <isorochan@gmail.com>
2023-08-12 22:36:36 +0300

    Init commit

---
+++ beholder.py
@@ -0,0 +1,55 @@
+"""Sets the path to the TOP folder so that the modules can be imported"""
+import sys
+import os
+
+# from the src folder
+# Windows: set PYTHONPATH=%PYTHONPATH%;C:.....\src
+# Linux: export PYTHONPATH=$PYTHONPATH:/home/.../src
+# Docker: ENV PYTHONPATH="./src"
+
+
+# src_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+src_folder = os.path.abspath(os.path.dirname(__file__))
+# project_path = os.path.abspath(os.path.join(os.path.dirname(src_folder)))
+other_folders = os.path.join(src_folder, "NLP")
+pathlist = [src_folder, other_folders]
+
+
+def add_sys_path(pathlist=pathlist):
+    """Add path to sys.path if it is not there"""
+    for path_name in pathlist:
+        if path_name not in sys.path:
+            sys.path.append(path_name)
+
+
+def remove_sys_path(pathlist=pathlist):
+    """Remove path from sys.path if it is there"""
+    for path_name in pathlist:
+        if path_name in sys.path:
+            sys.path.remove(path_name)
+
+
+add_sys_path(pathlist)
+# remove_sys_path(path_list)
+
+
+def get_variable_name(obj: object) -> str:
+    """Returns the name of the variable"""
+    for name, value in globals().items():
+        if id(value) == id(obj):
+            return name
+    return None
+
+
+def print_methods(obj: object) -> None:
+    """Returns all methods of an object"""
+    methods_ = [method_name for method_name in dir(obj) if method_name[0] != "_"]
+    print(
+        f"{len(methods_)} methods for {get_variable_name(obj)} ({obj.__class__}): \n {methods_}"
+    )
+
+
+if __name__ == "__main__":
+    add_sys_path()
+    for path in sys.path:
+        print(path)
